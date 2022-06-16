ba84bac4cce4094e3c8fa2ae91b0f0ad7c38838b
kintchidan <googeldan@gmail.com>
2022-06-16 18:06:21 +0300

    view

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+});
+Route::get('mod', function(){
+
+
+    return view('mohammedjjj ');
+
+
 });
